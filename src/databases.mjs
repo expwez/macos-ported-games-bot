0b103fc7d4d920296755b996816518b0c9afd23b
expwez <exp.wez@gmail.com>
2023-07-16 22:18:06 +0300

    Fix

--- src/databases.mjs
+++ src/databases.mjs
@@ -3,12 +3,10 @@ import { FileDatabase } from "./lib/fileDatabase.mjs";
 export const gamesDb = new FileDatabase('data/games.json');
 export const chatsDb = new FileDatabase('data/chats.json');
 
-
 export const getGameLatestRating = (game) => {
     if (game.ratings && game.ratings.length > 0) {
-        // Sort by date and take the latest record
-        game.ratings.sort((a, b) => new Date(b.addedAt) - new Date(a.addedAt));
-        return game.ratings[0].rating;
+        const sortedRatings = [...game.ratings].sort((a, b) => new Date(b.addedAt) - new Date(a.addedAt));
+        return sortedRatings[0].rating;
     } else {
         return null;
     }
